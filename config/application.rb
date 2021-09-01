29f7b826d347fdf4845569a76fae1aa06ea76c9d
monoue <like_a_newborn_baby@icloud.com>
2021-09-01 12:38:58 +0900

    add: Japanese version notice

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,8 @@ Bundler.require(*Rails.groups)
 
 module BooksApp
   class Application < Rails::Application
+    config.i18n.default_locale = :ja
+    config.i18n.available_locales = %i[en ja]
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
