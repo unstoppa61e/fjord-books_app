b503c79606ceaa14e5369395038435571098ce34
monoue <like_a_newborn_baby@icloud.com>
2021-09-28 11:34:33 +0900

    add: null check for comment's user id

--- db/migrate/20210914233146_create_comments.rb
+++ db/migrate/20210914233146_create_comments.rb
@@ -4,7 +4,7 @@ class CreateComments < ActiveRecord::Migration[6.1]
   def change
     create_table :comments do |t|
       t.references :commentable, polymorphic: true, null: false
-      t.integer :user_id
+      t.integer :user_id, null: false
       t.text :body
 
       t.timestamps
