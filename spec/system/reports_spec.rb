1937cd766be0bb8204de02ce9bce7edb60283936
monoue <like_a_newborn_baby@icloud.com>
2021-10-07 12:04:18 +0900

    削除: Report 作成時の画面表示のテストにて、過剰な部分

--- spec/system/reports_spec.rb
+++ spec/system/reports_spec.rb
@@ -19,10 +19,7 @@ RSpec.describe 'Reports', type: :system do
 
     expect(page).to have_content I18n.t('views.common.title_show', name: Report.model_name.human)
     expect(page).to have_content I18n.t('controllers.common.notice_create', name: Report.model_name.human)
-    expect(page).to have_content "#{Report.human_attribute_name(:title)}: #{title}"
-    expect(page).to have_content "#{Report.human_attribute_name(:content)}:"
     expect(page).to have_content content
-    expect(page).to have_content "#{Report.human_attribute_name(:user)}: "
     expect(page).to have_link user.name, href: user_path(user)
     expect(page).to have_content "#{Report.human_attribute_name(:created_on)}: #{I18n.l(Report.last.created_on)}"
     expect(page).to have_content "#{Comment.model_name.human}: （コメントがありません）"
